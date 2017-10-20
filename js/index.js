a77241fe46d9671e80f6b4c42e2dd1a3d27365ab
gomasiwo <gomashiwo.kou@gmail.com>
2017-10-20 21:33:22 +0900

    gomasiwo

--- js/index.js
+++ js/index.js
@@ -1,21 +1,47 @@
 $ (function(){
-	var time = 500;
+	var time = 400;
 	$("a#arrdwn").hover(function(){
 		$(this).animate({opacity: 0.25}, time, 'easeOutQuad');
 	},function(){
-		$(this).animate({opacity: 1}, time, 'easeOutQuart');
+		$(this).animate({opacity: 1}, time/2, 'easeOutQuart');
 	});
 });
 
 $(function(){
-	var time = 1000;
+	var time = 800;
 			
 	$('a[href^=#]').click(function(){
 		var target = $(this.hash);
 		if(!target.length) return;
-		var targetY = target.offset().top
+		var targetY = target.offset().top;
 		$('html,body').animate({scrollTop: targetY}, time, 'easeOutQuad');
 		window.history.pushState(null,null,null,this.hash);
 		return false;
 	});
 });
+
+$(function(){
+	var contents = $('#about').offset().top;
+	$(window).on('scroll',function(){
+		var winTop = $(window).scrollTop();
+		if(winTop >= contents){
+			$('#nav li a').css('color','#353535');
+		}else if(winTop <= contents){
+			$('#nav li a').css('color','#fff');
+		}
+	});
+});
+
+/*
+$(function(){// 変数『obj_t_pos』に位置を代入
+	$(window).scroll(function(){
+		var obj_t_pos = $(window).scrollTop(); 
+		$('#target').html(obj_t_pos); 
+		if(obj_t_pos > 900){
+			$('#target').addClass('gray');
+		}else{
+			$('#target').removeClass();
+		}
+	});
+});
+*/
