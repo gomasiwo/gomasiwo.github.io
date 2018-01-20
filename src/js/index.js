f525b89243716b48690690f16367294cd34b72c9
gomasiwo <gomashiwo.kou@gmail.com>
2018-01-21 03:31:18 +0900

    gomashiwo

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,36 @@
 // import文を使ってSassファイルを読み込む。
 import "../css/index.scss"
+import $ from 'jquery'
+
+const Chaffle = require("chaffle")
+
+$(function () {
+    var elements = document.querySelectorAll('[data-chaffle]');
+    var elm = document.querySelectorAll('[data-chaffle-onLoad]');
+
+    Array.prototype.forEach.call(elements, function (el) {
+        var chaffle = new Chaffle(el)
+        el.addEventListener('mouseover', function () {
+            chaffle.init();
+        });
+    });
+
+    Array.prototype.forEach.call(elm, function (el) {
+        var chaffle = new Chaffle(el, {
+            delay: 200
+        })
+        setInterval(function () {
+            chaffle.init();
+        }, 8000)
+    });
+});
+
+$(function () {
+    $('#wapper').css('display', 'none');
+    $('#loading').css('display', 'none');
+})
+
+$(window).load(function () {
+    $('#wapper').css('display', 'flex');
+    $('#loading').css('display', 'none');
+})
